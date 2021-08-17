7e04b0e3137eae4ee1121511ad000b2bca71f899
Mikael Lindh <mslindh@gmail.com>
2021-08-17 15:29:15 +0200

    Added toolbar stub.

---
+++ app/src/main/java/funkyburger/the5000/widget/MainToolBar.java
@@ -0,0 +1,23 @@
+package funkyburger.the5000.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Toolbar;
+
+public class MainToolBar extends Toolbar {
+    public MainToolBar(Context context) {
+        super(context);
+    }
+
+    public MainToolBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MainToolBar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public MainToolBar(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
